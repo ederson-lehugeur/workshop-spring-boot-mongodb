6b73d30dea093157ac01c7307474f3599c57076a
Éderson Lehugeur <eder.lehugeur@gmail.com>
2020-02-07 18:16:53 -0300

    Atualização de usuário com PUT

--- src/main/java/br/com/workshopmongodb/resources/UserResource.java
+++ src/main/java/br/com/workshopmongodb/resources/UserResource.java
@@ -58,6 +58,18 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDto) {
+
+		User user = userService.fromDTO(userDto);
+
+		user.setId(id);
+
+		user = userService.update(user);
+
+		return ResponseEntity.noContent().build();
+	}
+
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 
