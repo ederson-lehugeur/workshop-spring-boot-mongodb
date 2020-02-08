6b8934fc0c8599f0178ee849ccda8f32f4d559d5
Éderson Lehugeur <eder.lehugeur@gmail.com>
2020-02-08 17:57:44 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/workshopmongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package br.com.workshopmongodb.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.workshopmongodb.domains.Post;
+import br.com.workshopmongodb.repositories.PostRepository;
+import br.com.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		return postRepository.findById(id)
+				.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
